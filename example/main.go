64c727e1455ec4bba5b616524cd710a046c211b0
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-03-13 09:02:15 -0400

    updated to use latest go protobuf (and not gogo protobuf)

--- example/main.go
+++ example/main.go
@@ -4,34 +4,39 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"os"
 	"reflect"
 
-	"github.com/gogo/protobuf/proto"
+	proto "google.golang.org/protobuf/proto"
+
 	"github.com/jimlambrt/any"
+	"github.com/jimlambrt/any/any_test"
 )
 
 func main() {
 	types, _ := any.NewTypeCatalog(
-		new(any.TestUser),
-		new(any.TestCar),
-		new(any.TestRental),
+		new(any_test.TestUser),
+		new(any_test.TestCar),
+		new(any_test.TestRental),
 		new(TestShirt),
 		new(TestPants),
 	)
 	queue := any.Queue{Catalog: types}
 
-	user := any.TestUser{
+	user := any_test.TestUser{
 		Id:          1,
 		Name:        "Alice",
 		PhoneNumber: "867-5309",
 		Email:       "alice@bob.com",
 	}
-	car := any.TestCar{
+	car := any_test.TestCar{
 		Id:    2,
 		Model: "Jeep",
 		Mpg:   25,
 	}
-	rental := any.TestRental{
+	rental := any_test.TestRental{
 		UserId: 1,
 		CarId:  2,
 	}
@@ -49,26 +54,26 @@ func main() {
 		Material: "denim",
 	}
 
-	queue.Add(&user, &car, &rental, &shirt, &pants)
+	_ = queue.Add(&user, &car, &rental, &shirt, &pants)
 
 	queuedUser, _ := queue.Remove()
 	origUser, _ := comparable(&user)
 	printJSON(queuedUser, origUser)
-	if !reflect.DeepEqual(*origUser.(*any.TestUser), *queuedUser.(*any.TestUser)) {
+	if !reflect.DeepEqual(*origUser.(*any_test.TestUser), *queuedUser.(*any_test.TestUser)) {
 		panic("users should be equal")
 	}
 
 	queuedCar, _ := queue.Remove()
-	origCar, err := comparable(&car)
+	origCar, _ := comparable(&car)
 	printJSON(queuedCar, origCar)
-	if !reflect.DeepEqual(*origCar.(*any.TestCar), *queuedCar.(*any.TestCar)) {
+	if !reflect.DeepEqual(*origCar.(*any_test.TestCar), *queuedCar.(*any_test.TestCar)) {
 		panic("cars should be equal")
 	}
 
 	queuedRental, _ := queue.Remove()
 	origRental, _ := comparable(&rental)
 	printJSON(queuedRental, origRental)
-	if !reflect.DeepEqual(*origRental.(*any.TestRental), *queuedRental.(*any.TestRental)) {
+	if !reflect.DeepEqual(*origRental.(*any_test.TestRental), *queuedRental.(*any_test.TestRental)) {
 		panic("rentals should be equal")
 	}
 
@@ -84,11 +89,40 @@ func main() {
 		panic("pants should be equal")
 	}
 
-	_, err = queue.Remove()
+	_, err := queue.Remove()
 	if err != io.EOF {
 		panic("should get EOF")
 	}
 
+	wfileQueue := any.Queue{Catalog: types}
+	if err := wfileQueue.Add(&user, &car); err != nil {
+		panic(err)
+	}
+
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(wfileQueue.QueueBuffer); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+	_ = tmpfile.Close()
+
+	rfileQueue := any.Queue{Catalog: types}
+	in, err := ioutil.ReadFile(tmpfile.Name())
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	rfileQueue.QueueBuffer = in
+	fileUser, _ := rfileQueue.Remove()
+	fmt.Println("from file.")
+	printJSON(fileUser, origUser)
+	if !reflect.DeepEqual(*origUser.(*any_test.TestUser), *fileUser.(*any_test.TestUser)) {
+		panic("users should be equal")
+	}
 }
 
 type TestShirt struct {
@@ -117,7 +151,6 @@ func comparable(m proto.Message) (proto.Message, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	retMsg := reflect.New(reflect.TypeOf(m).Elem()).Interface().(proto.Message)
 	if err := proto.Unmarshal(data, retMsg); err != nil {
 		return nil, err
